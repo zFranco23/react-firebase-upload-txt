f3aeefd4fe70d3498323b211cbf186f240cf1c48
zFranco23 <francoheflo@gmail.com>
2022-08-25 23:14:32 -0500

    Added multiupload approach

--- src/components/CustomUpload.tsx
+++ src/components/CustomUpload.tsx
@@ -1,10 +1,39 @@
-import { FC } from 'react'
+import { ChangeEvent, FC } from 'react'
+import { multiUpload } from '../firebase';
+import { ICustomDocument } from '../interfaces';
 
 
 const CustomUpload: FC = () => {
+
+    const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e && e.target) {
+            if (e.target.files) {
+                const file = e.target.files[0];
+                const reader = new FileReader();
+                reader.onload = function (progressEvent: ProgressEvent) {
+                    if (typeof this.result === 'string') {
+                        const arrStrings = this.result.split('\n')
+                        const cleanLines = arrStrings.filter((s: string) => s.trim().length)
+
+                        //Make your own schema
+                        const docsToUpload: ICustomDocument[] = cleanLines.map((line: string) => ({
+                            id: line.trim(),
+                            createdAt: new Date().getTime(),
+                            alreadyUsed: false,
+                        }))
+
+                        multiUpload(docsToUpload);
+                    }
+                };
+                reader.readAsText(file);
+            }
+        }
+    }
+
     return (
         <div>
             <h1>Custom upload</h1>
+            <input type="file" accept="text/plain" onChange={onFileChange} />
         </div>
     )
 }
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,2 +1,23 @@
 import { db } from "./firebase.config";
+import { doc, setDoc } from "firebase/firestore";
+import { IDocument } from "../interfaces";
+
+const PATH = 'test';
+
+const uploadDocument = (document: IDocument, id: string) => {
+    const docRef = doc(db, PATH, id);
+    return setDoc(docRef, document);
+}
+
+export const multiUpload = async (docs: IDocument[]) => {
+    const promises = docs.map((doc: IDocument, idx: number) => uploadDocument(doc, doc.id))
+    const resp = await Promise.allSettled(promises);
+    let failed: number[] = [];
+    resp.forEach((st: PromiseSettledResult<void>, idx) => {
+        if (st.status === 'rejected') failed.push(idx);
+    })
+    failed.forEach((v, i) => {
+        console.log('[FAIL] Upload doc failed : ', docs[v]);
+    })
+}
 
