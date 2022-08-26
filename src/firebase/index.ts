dcba2d5c603ad8ca638550f3613bc3df5edf558e
zFranco23 <francoheflo@gmail.com>
2022-08-25 23:56:32 -0500

    Added batch upload simple approach

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,8 +1,9 @@
 import { db } from "./firebase.config";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, writeBatch, setDoc } from "firebase/firestore";
 import { IDocument } from "../interfaces";
 
 const PATH = 'test';
+const MAX_FIREBASE_DOCS_PER_BATCH = 500;
 
 const uploadDocument = (document: IDocument, id: string) => {
     const docRef = doc(db, PATH, id);
@@ -20,4 +21,28 @@ export const multiUpload = async (docs: IDocument[]) => {
         console.log('[FAIL] Upload doc failed : ', docs[v]);
     })
 }
+
+//MAXIMUM 500 OPERATIONS PER BATCH
+const batchPartitionUpload = async (partialDocs: IDocument[]) => {
+    const batchHandler = writeBatch(db);
+    partialDocs.forEach((d: IDocument, idx: number) => {
+        const docRef = doc(db, PATH, d.id);
+        batchHandler.set(docRef, d);
+    })
+    await batchHandler.commit();
+}
+
+export const batchUpload = async (docs: IDocument[]) => {
+    const chunkSize = docs.length > MAX_FIREBASE_DOCS_PER_BATCH
+        ? Math.ceil(docs.length / MAX_FIREBASE_DOCS_PER_BATCH)
+        : 1;
+    for (let i = 0; i < chunkSize; i++) {
+        const init = i * MAX_FIREBASE_DOCS_PER_BATCH;
+        const end = (i + 1) * MAX_FIREBASE_DOCS_PER_BATCH - 1;
+        batchPartitionUpload(docs.slice(init, end + 1));
+        console.log('PARTITION : ',docs.slice(init, end + 1))
+    }
+
+}
+
 
