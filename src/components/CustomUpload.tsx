16357405d2a3b228d0ee98ad47bc29461d039332
zFranco23 <francoheflo@gmail.com>
2022-08-26 03:26:21 -0500

    Initial commit

---
+++ src/components/CustomUpload.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+
+const CustomUpload: FC = () => {
+    return (
+        <div>
+            <h1>Custom upload</h1>
+        </div>
+    )
+}
+
+export default CustomUpload
